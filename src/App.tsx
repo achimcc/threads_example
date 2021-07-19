af28cb1eedb0a09000fb92944bcf166c0659fa44
achimcc <achim@parity.io>
2021-07-19 22:12:39 +0200

    remove noEmit, cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
 const App = () => <h1>Basic TypeScript App</h1>;
 
 export default App;
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,6 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-/**
- * @jest-environment jsdom
- */
 test("renders Basic TypeScript App", () => {
   render(<App />);
   const linkElement = screen.getByText(/Basic TypeScript App/i);
