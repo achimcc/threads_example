0899b175926ba11387da338bf91695034413fbd9
achimcc <achim@parity.io>
2021-07-20 13:43:58 +0200

    add import od WorkerURL

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { spawn, Thread, Worker } from "threads";
+// @ts-ignore
+import workerURL from "threads-plugin/dist/loader?name=gitWorker!./workers/add.ts";
 
 async function runWorker() {
-  const add = await spawn(
-    new Worker(new URL("./workers/add", import.meta.url).href)
-  );
+  const add = await spawn(new Worker(workerURL));
   const sum = await add(2, 3);
 
   console.log(`2 + 3 = ${sum}`);
